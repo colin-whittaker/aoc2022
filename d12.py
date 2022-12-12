7cf38113b9693d324ac6180a5a8f2731f8b5b408
colin whittaker <colin+github@netech.ie>
2022-12-12 09:12:05 +0000

    day 12

---
+++ d12.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3.10
+import sys
+import heapq
+
+def valid_step(a,b):
+    a = 0 if a == 'S' else ord(a) - ord('a')
+    b = 25 if b == 'E' else ord(b) - ord('a')
+    return True if a+1 >= b else False
+
+def solve_grid(start,finish,grid):
+    costs = [(0,(start))]
+    visit = {}
+    visit[start] = 0
+    xmax,ymax = len(grid[0]),len(grid)
+    directions = [(-1,0),(1,0),(0,-1),(0,1)]
+    while True:
+        if len(costs) == 0:
+            return 10000000
+        score, npos = heapq.heappop(costs)
+        if npos == finish:
+            break
+        x,y = npos
+        for i,j in directions:
+            a,b = x+i,y+j
+            if 0 <= a < xmax and 0 <= b < ymax and valid_step(grid[y][x],grid[b][a]):
+                if (a,b) not in visit or score+1 < visit[(a,b)]:
+                    heapq.heappush(costs,(score+1,(a,b)))
+                    visit[(a,b)] = score + 1
+    return(score)
+
+if __name__ == '__main__':
+    lines = list(line.rstrip() for line in sys.stdin)
+    #do stuff
+    grid = []
+    for line in lines:
+        grid.append(line)
+    start, finish = None,None
+    for y in range(0,len(grid)):
+        if 'S' in grid[y]:
+            start = (grid[y].find('S'),y)
+        if 'E' in grid[y]:
+            finish = (grid[y].find('E'),y)
+
+    score = solve_grid(start,finish,grid)
+    print(score)
+    for y,row in enumerate(grid):
+        for x in [i for i, letter in enumerate(row) if letter == 'a']:
+            result = solve_grid((x,y),finish,grid)
+            score = result if result < score else score
+    print(score)
+                
+
