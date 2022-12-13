d4ec6918562706b1ba1c8803c79c5952aec781d8
colin whittaker <colin+github@netech.ie>
2022-12-13 12:32:53 +0000

    d13 cleaned up and fixed the input parsing bug

--- d13.py
+++ d13.py
@@ -5,19 +5,16 @@ import sys
 
 def check_pair(left,right):
     if type(left) == type(right) == int:
-        if left < right: return 1
-        elif left > right: return -1
-        else: return 0
+        if left < right: return True
+        elif left > right: return False
     elif type(left) == type(right) == list:
         for i in range(min(len(left),len(right))):
             result = check_pair(left[i],right[i])
-            if result != 0:  return result
+            if result != None:  return result
         if len(right) > len(left):
-            return 1
+            return True
         elif len(right) < len(left):
-            return -1
-        else:
-            return 0
+            return False
     elif type(left) == int:
         return check_pair([left],right)
     elif type(right) == int:
@@ -26,32 +23,23 @@ def check_pair(left,right):
 def pkt_sort(pkts):
     n = len(pkts)
     for i in range(n-1):
-        for j in range(0, n-i-1):
-            if check_pair(pkts[j+1], pkts[j]) == 1:
+        for j in range(n-i-1):
+            if check_pair(pkts[j+1], pkts[j]):
                 pkts[j],pkts[j+1] = pkts[j+1], pkts[j]
 
 if __name__ == '__main__':
     lines = list(line.rstrip() for line in sys.stdin)
     #do stuff
-    pairs = []
+    pairs = [[]]
     pkts = [[[2]],[[6]]]
-    left,right = None,None
     for line in lines:
         if line == '':
-            pairs.append((left,right))
-            left,right = None, None
-            continue
-        if left == None:
-            left = ast.literal_eval(line)
-            pkts.append(left)
+            pairs.append([])
         else:
-            right = ast.literal_eval(line)
-            pkts.append(right)
-    score = 0
-    for i in range(0,len(pairs)):
-        if check_pair(*pairs[i]) == 1:
-            score += i+1
-    print(score)
+            pairs[-1].append( ast.literal_eval(line) )
+            pkts.append(pairs[-1][-1])
+
+    print(sum([i+1 for i in range(0,len(pairs)) if check_pair(pairs[i][0],pairs[i][1])]))
 
     pkt_sort(pkts)
     print(math.prod([i+1 for i,p in enumerate(pkts) if (p == [[2]] or p == [[6]])]))
